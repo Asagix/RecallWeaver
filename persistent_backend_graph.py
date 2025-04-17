02b9f5a53c4e46ca8155fe0bafe7dda291877245
Asagix <CyanAsagi@proton.me>
2025-04-17 14:22:04 +0200

    feat: Add emoji stripping helper regex

--- persistent_backend_graph.py
+++ persistent_backend_graph.py
@@ -23,6 +23,28 @@ from datetime import datetime, timezone, timedelta
 
 from workspace_agent import WorkspaceAgent
 
+# --- Emoji Stripping Helper ---
+# Regex to match most common emojis
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "\U000024C2-\U0001F251"
+    "\U00002600-\U000026FF"  # Miscellaneous Symbols
+    "\U00002B50"            # Star
+    "\U0000FE0F"            # Variation selector
+    "\U0001F004"            # Mahjong tile red dragon
+    "\U0001F0CF"            # Playing card joker
+    "]+", flags=re.UNICODE)
+
 # Import zoneinfo safely
 try:
     from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
